24f6a72f7f1b950c989a7b8a64df3cb6f403e173
Nikola Lajic <n.lajic@gmail.com>
2017-12-02 11:14:42 +0100

    Initial commit

---
+++ Assets/ControlledMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ControlledMovement : MonoBehaviour {
+
+    public float speed = 1;
+    Rigidbody2D rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate () {
+        float vertical = Input.GetAxis("Vertical") * speed;
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        rb.MovePosition((Vector2)transform.position + new Vector2(horizontal, vertical) * Time.deltaTime);
+	}
+}
