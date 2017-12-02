22026577acb313e7f3328d287329bd9834ecd495
Nikola Lajic <n.lajic@gmail.com>
2017-12-02 12:13:50 +0100

    Added goal

---
+++ Assets/Goal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+    List<GameObject> allCats = new List<GameObject>();
+    List<GameObject> herderdCats = new List<GameObject>();
+	
+	void Start () {
+        allCats.AddRange(GameObject.FindGameObjectsWithTag("Cat")); 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Cat") {
+            herderdCats.Add(collision.gameObject);
+            if (herderdCats.Count == allCats.Count) {
+                Debug.Log("You win");
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.tag == "Cat") {
+            herderdCats.Remove(collision.gameObject);
+        } 
+    }
+}
