96a8f40365278dc922b33b801c86c3c59250bbba
Nikola Lajic <n.lajic@gmail.com>
2017-12-02 12:29:08 +0100

    Added level timer

---
+++ Assets/LevelTimer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelTimer : MonoBehaviour {
+
+	public Text label;
+    public int timeLimit = 30;
+    Coroutine tick;
+
+    private void Start() {
+        Countdown(timeLimit);
+    }
+
+    public void Countdown(int seconds) {
+        if (tick != null) StopCoroutine(tick);
+        tick = StartCoroutine(Tick(seconds));
+        UpdateText(seconds);
+    }
+	
+    IEnumerator Tick(int seconds) {
+        while (seconds > 0) {
+            seconds--;
+			yield return new WaitForSeconds(1);
+            UpdateText(seconds);
+        }
+        Debug.Log("Out of time");
+    }
+
+    void UpdateText(int seconds) {
+        label.text = seconds.ToString();
+    }
+}
