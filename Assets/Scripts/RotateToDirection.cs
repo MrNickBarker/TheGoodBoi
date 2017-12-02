b3f37087525be81a98b538c70b6ca99210af1880
Nikola Lajic <n.lajic@gmail.com>
2017-12-02 19:34:30 +0100

    Added dog model, model rotation

---
+++ Assets/Scripts/RotateToDirection.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RotateToDirection : MonoBehaviour {
+
+    public GameObject model;
+    Vector3 lastPosition = Vector2.zero;
+    	
+	void FixedUpdate () {
+        if (lastPosition == transform.position) return;
+        Vector3 dir = lastPosition - transform.position;
+        float towards = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        Vector3 angles = model.transform.eulerAngles;
+        angles.z = towards;
+        model.transform.eulerAngles = angles;
+		lastPosition = transform.position;
+	}
+}
