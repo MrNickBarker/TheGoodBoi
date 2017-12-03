68f5735c0da4a0de717028db0bec3af437e38854
Nikola Lajic <n.lajic@gmail.com>
2017-12-03 23:26:39 +0100

    Dog animation

---
+++ Assets/Scripts/MoveAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAnimation : MonoBehaviour {
+
+    public Animator animator;
+    Rigidbody2D rb;
+    bool walking = false;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update () {
+        bool shouldWalk = rb.velocity.magnitude > 0.2f;
+        if (shouldWalk != walking) {
+            walking = shouldWalk;
+            animator.SetBool("Walking", walking);
+        }
+	}
+}
