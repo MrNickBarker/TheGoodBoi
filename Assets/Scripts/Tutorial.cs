8faaaecb27e9e5fa1bdfab29ce4c440cc7116349
Nikola Lajic <n.lajic@gmail.com>
2017-12-03 22:57:33 +0100

    Tutorial

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Tutorial : MonoBehaviour {
+	
+	void Update () {
+        if (Mathf.Approximately(Input.GetAxis("Horizontal"), 0) == false ||
+            Mathf.Approximately(Input.GetAxis("Vertical"), 0) == false ||
+            Input.GetKeyDown(KeyCode.Space)) {
+            Destroy(gameObject);
+        }
+	}
+}
