c1e326cb799377ef8f3fc6cd879fea0ba49771f7
Nikola Lajic <n.lajic@gmail.com>
2017-12-03 20:47:34 +0100

    Credits scene

---
+++ Assets/Scripts/CreditsController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsController : MonoBehaviour {
+
+    public void OnRestart() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OnWebsite() {
+        Application.OpenURL("http://www.pixelome.com/");
+    }
+
+    public void OnTwitter() {
+        Application.OpenURL("http://www.twitter.com/pixelome/");
+    }
+}
