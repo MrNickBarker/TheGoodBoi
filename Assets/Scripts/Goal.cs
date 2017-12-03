71a72d933a3023e5b9d78fc6fcf92cf5e22f5bf4
Nikola Lajic <n.lajic@gmail.com>
2017-12-03 23:54:10 +0100

    Dumpster sound

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -5,13 +5,16 @@ public class Goal : MonoBehaviour {
 
     List<GameObject> allCats = new List<GameObject>();
     List<GameObject> herderdCats = new List<GameObject>();
+    AudioSource sound;
 	
 	void Start () {
-        allCats.AddRange(GameObject.FindGameObjectsWithTag("Cat")); 
+        allCats.AddRange(GameObject.FindGameObjectsWithTag("Cat"));
+        sound = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "Cat") {
+            sound.Play();
             herderdCats.Add(collision.gameObject);
             if (herderdCats.Count == allCats.Count) {
                 FindObjectOfType<LevelController>().OnLevelComplete();
