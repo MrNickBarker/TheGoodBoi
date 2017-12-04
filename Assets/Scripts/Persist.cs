cbe471d065d5d0a1c2591cda1cceb1d60da757d6
Nikola Lajic <n.lajic@gmail.com>
2017-12-04 01:26:19 +0100

    Wind

---
+++ Assets/Scripts/Persist.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Persist : MonoBehaviour {
+
+    public void Awake() {
+        DontDestroyOnLoad(this);
+
+        if (FindObjectsOfType(GetType()).Length > 1) {
+            Destroy(gameObject);
+        }
+    }
+}
